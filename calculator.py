0142f8dee921cfc173d3b6d0f4fdaf3f71b455ce
bhautik12345 <220130107015@gecg28.ac.in>
2025-09-03 12:53:29 +0530

    calculator

---
+++ calculator.py
@@ -0,0 +1,26 @@
+class calc:
+    def sums(self, *args):
+        print("sum is :",sum(args))
+        
+    def multi(self, *args):
+        res = 1
+        for ele in args:
+            res = res * ele
+        print("multiplication is :",res)
+
+    def division(self, a, b):
+        print("division is :",a/b)
+
+    def subtraction(self, a, b):
+        print("subtraction is :",a-b)
+        
+    def exponentiation(self, a, pow):
+        print("expo is :",a**pow)
+    
+obj = calc()
+obj.sums(1,2,3,4,5)
+obj.multi(1,2,3)
+obj.division(5,4)
+obj.subtraction(5,6)
+obj.exponentiation(2,5)
+
