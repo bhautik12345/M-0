69f70b34205b8e58d7b20157733770a127847f1a
bhautik12345 <220130107015@gecg28.ac.in>
2025-09-03 12:58:53 +0530

    merge two sorted list

---
+++ merge_list.py
@@ -0,0 +1,25 @@
+def merge_list(list1, list2):
+    merged_list = []
+    pt1 = 0
+    pt2 = 0
+
+    while (pt1 < len(list1) and pt2 < len(list2)):
+        if list1[pt1] < list2[pt2]:
+            merged_list.append(list1[pt1])
+            pt1 += 1
+        else:
+            merged_list.append(list2[pt2])
+            pt2 +=1
+
+    if pt1 < len(list1):
+        merged_list.append(list1[pt1])
+
+    if pt2 < len(list2):
+        merged_list.append(list2[pt2])
+
+    return merged_list
+
+l1 = [1,3,5,7]
+l2 = [2,4,6,8]
+
+print(merge_list(l1, l2))
