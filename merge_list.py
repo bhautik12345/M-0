feb6f428a6477889be794f41f9298ecc3f978422
bhautik12345 <220130107015@gecg28.ac.in>
2025-09-04 11:49:24 +0530

    final commit of merge_list

--- merge_list.py
+++ merge_list.py
@@ -11,15 +11,17 @@ def merge_list(list1, list2):
             merged_list.append(list2[pt2])
             pt2 +=1
 
-    if pt1 < len(list1):
+    while pt1 < len(list1):
         merged_list.append(list1[pt1])
+        pt1 += 1
 
-    if pt2 < len(list2):
+    while pt2 < len(list2):
         merged_list.append(list2[pt2])
+        pt2 += 1
 
     return merged_list
 
-l1 = [1,3,5,7]
+l1 = [1,3,5,7,9,10]
 l2 = [2,4,6,8]
 
 print(merge_list(l1, l2))
